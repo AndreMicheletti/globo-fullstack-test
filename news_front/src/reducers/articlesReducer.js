0b9587208b229953075512506d7c736237069480
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2020-07-08 12:00:10 -0300

    added redux and redux-thunk

---
+++ news_front/src/reducers/articlesReducer.js
@@ -0,0 +1,12 @@
+
+const INITIAL_STATE = []
+
+const articlesReducer = (state = INITIAL_STATE, action) => {
+    console.log(action)
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default articlesReducer
